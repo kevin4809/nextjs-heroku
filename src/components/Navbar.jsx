b262071df1d9b397d68d432bce37fd4174cf6c27
Kevin Smith Jimenez Garro <kevinedk33@gmail.com>
2024-03-20 10:52:00 -0500

    1
    1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div className="bg-slate-500 ">
       <div className="max-w-[1300px] h-[50px]  flex items-center justify-start gap-[50px] w-[90%] m-auto">
         <Link href="/">Home</Link>
-        <Link href="/pagetwo">Pagina dos</Link>
+        <Link href="/pageTwo">Pagina dos</Link>
       </div>
     </div>
   );
