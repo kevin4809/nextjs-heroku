ba6474e3c3f208d1d89c44af06c65b5af571d1cd
Kevin Smith Jimenez Garro <kevinedk33@gmail.com>
2024-03-20 10:23:13 -0500

    2
    2

---
+++ src/app/pageTwo/components/Table.jsx
@@ -0,0 +1,72 @@
+"use client"
+
+
+import React, { useState } from "react";
+
+const Table = () => {
+  const [changePrices, setchangePrices] = useState(["$2999", "$1999", "99"]);
+
+  const changePricesFuntion = () => {
+    if (changePrices[0] === "$2999") {
+      setchangePrices(["$5000", "$124124", "$4121"]);
+    } else {
+      setchangePrices(["$2999", "$1999", "99"]);
+    }
+  };
+  return (
+    <div className="relative overflow-x-auto">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              Product name
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Color
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Category
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Price
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              Apple MacBook Pro 17"
+            </th>
+            <td className="px-6 py-4">Silver</td>
+            <td className="px-6 py-4">Laptop</td>
+            <td className="px-6 py-4">{changePrices[0]}</td>
+          </tr>
+          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              Microsoft Surface Pro
+            </th>
+            <td className="px-6 py-4">White</td>
+            <td className="px-6 py-4">Laptop PC</td>
+            <td className="px-6 py-4">{changePrices[1]}</td>
+          </tr>
+          <tr className="bg-white dark:bg-gray-800">
+            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+              Magic Mouse 2
+            </th>
+            <td className="px-6 py-4">Black</td>
+            <td className="px-6 py-4">Accessories</td>
+            <td className="px-6 py-4">{changePrices[2]}</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div className="w-full flex justify-center mt-[40px]">
+        <button className="bg-slate-500 p-2" onClick={changePricesFuntion}>
+          Cambiar precios
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Table;
